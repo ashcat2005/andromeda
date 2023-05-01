e85a93c94e383352a67f74f49b28ba8d24e3f1d7
Eduard Larranaga <eduard@Eduards-MacBook-Air.local>
2023-05-01 13:10:37 -0500

    The config.py file is deprecated. Only the main.py is used now.

--- main.py
+++ main.py
@@ -6,11 +6,63 @@ Creates the Black Hole image
 @author: Eduard Larrañga - 2023
 ===============================================================================
 """
+#import warnings
+#warnings.filterwarnings('ignore')
 
+from numpy import pi
+#from metrics import schwarzschild
+from metrics import kerr
+from accretion_structures import thin_disk 
+from detectors import image_plane 
 from common.common import Image
-from config import *
-import warnings
-warnings.filterwarnings('ignore')
+
+
+
+
+
+###############################################################################
+############################ BLACK HOLE DEFINITION ############################
+###############################################################################
+M = 1 # Mass
+a = 0.5 # Angular Monmentum
+#blackhole = schwarzschild.BlackHole(M)
+blackhole = kerr.BlackHole(M,a)
+
+
+###############################################################################
+############################# DETECTOR PARAMETERS #############################
+###############################################################################
+D = 100*M
+iota = pi/2.3
+x_screen_side = 25*M
+y_screen_side = 15*M
+n_pixels = 5
+detector = image_plane.detector(D=D, iota = iota, 
+                       x_s_side = x_screen_side, y_s_side = y_screen_side,
+                       n_pixels=n_pixels)
+
+
+###############################################################################
+############################# ACCRETION STRUCTURE #############################
+###############################################################################
+#R_min = blackhole.ISCOco 
+#R_max = 20*M
+acc_structure = thin_disk.structure(blackhole)#, R_min = 10)
+
+
+###############################################################################
+############################### IMAGE FILENAME ################################
+###############################################################################
+filename = 'NovikovThorneBlackHole'
+savefig = False
+
+
+
+
+
+
+
+
 
 #################################### MAIN #####################################
 
